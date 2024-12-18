37da13afe5c78dc48818a79ff8799f1a03eed275
Artur Makarov <Aru2208@bk.ru>
2024-12-18 11:13:14 +0300

    oop

--- main.py
+++ main.py
@@ -1,173 +1,203 @@
-import telegram
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
 import os
 import requests
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
 
-FIXER_API_KEY = ''
-TOKEN = ''
+
+FIXER_API_KEY = 'HezvO50ITvf0WNf01uNeDYfQPASUjFOw'
+TOKEN = '7766992589:AAGZVx_Jsd086BmLRMo8PwEmv0aCkaXMqos'
 FIXER_BASE_URL = 'https://api.apilayer.com/fixer/'
 HEADERS = {
     'apikey': FIXER_API_KEY
 }
 
-def get_available_currencies():
-    response = requests.get(f"{FIXER_BASE_URL}symbols", headers=HEADERS)
-    data = response.json()
-    if data.get("success"):
-        return data['symbols']
-    else:
-        return None
 
-def get_currency_rate(base: str, symbol: str):
-    response = requests.get(f"{FIXER_BASE_URL}latest", headers=HEADERS, params={
-        'base': base,
-        'symbols': symbol
-    })
-    data = response.json()
-    if data.get("success"):
-        return data['rates'].get(symbol)
-    else:
-        return None
+class CurrencyAPI:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.base_url = FIXER_BASE_URL
+        self.headers = {'apikey': self.api_key}
 
-def get_historical_data(base: str, symbol: str, days: int):
-    rates = []
-    dates = []
-    for i in range(days):
-        date = (datetime.now() - timedelta(days=i)).strftime('%Y-%m-%d')
-        response = requests.get(f"{FIXER_BASE_URL}{date}", headers=HEADERS, params={
+    def get_available_currencies(self):
+        response = requests.get(f"{self.base_url}symbols", headers=self.headers)
+        data = response.json()
+        return data['symbols'] if data.get("success") else None
+
+    def get_currency_rate(self, base, symbol):
+        response = requests.get(f"{self.base_url}latest", headers=self.headers, params={
             'base': base,
             'symbols': symbol
         })
         data = response.json()
-        if data.get("success"):
-            rates.append(data['rates'].get(symbol))
-            dates.append(date)
-    return rates[::-1], dates[::-1]
+        return data['rates'].get(symbol) if data.get("success") else None
 
-def plot_currency_rates(dates, rates, base, symbol):
-    plt.style.use('dark_background')
-    plt.figure(figsize=(12, 6))
-    formatted_dates = [datetime.strptime(date, '%Y-%m-%d').strftime('%m-%d') for date in dates]
-    plt.plot(formatted_dates, rates, marker='o', linestyle='-', color='#1f77b4', label=f'{base} to {symbol}')
-    plt.title(f"Exchange Rate: {base} to {symbol}", fontsize=16, color='white')
-    plt.xlabel("Date", fontsize=12, color='white')
-    plt.ylabel(f"Rate ({base} to {symbol})", fontsize=12, color='white')
-    plt.grid(visible=True, linestyle='--', alpha=0.5, color='gray')
-    plt.xticks(rotation=45, fontsize=10, color='white')
-    plt.yticks(fontsize=10, color='white')
-    plt.legend(fontsize=12, facecolor='black', edgecolor='white')
+    def get_historical_data(self, base, symbol, days):
+        rates, dates = [], []
+        for i in range(days):
+            date = (datetime.now() - timedelta(days=i)).strftime('%Y-%m-%d')
+            response = requests.get(f"{self.base_url}{date}", headers=self.headers, params={
+                'base': base,
+                'symbols': symbol
+            })
+            data = response.json()
+            if data.get("success"):
+                rates.append(data['rates'].get(symbol))
+                dates.append(date)
+        return rates[::-1], dates[::-1]
 
-    plot_path = "exchange_rate_plot.png"
-    plt.tight_layout()
-    plt.savefig(plot_path)
-    plt.close()
-    return plot_path
+    def calculate_trend(self, rates):
+        if len(rates) < 2:
+            return "Нет данных для анализа"
 
-async def rate_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    try:
-        base, symbol = context.args
-        rate = get_currency_rate(base.upper(), symbol.upper())
-        if rate:
-            keyboard = [[InlineKeyboardButton("🔄Обновить курс", callback_data=f"update_{base}_{symbol}")]]
-            reply_markup = InlineKeyboardMarkup(keyboard)
-            await update.message.reply_text(
-                text=f"Текущий курс конвертации {symbol.upper()} к {base.upper()}: {rate}",
-                reply_markup=reply_markup
-            )
+        if rates[-1] > rates[0]:
+            return "Растущий"
+
+        elif rates[-1] < rates[0]:
+            return "Падающий"
+
+        return "Стабильный"
+
+    def calculate_percentage_change(self, rates):
+        if len(rates) < 2:
+            return "Недостаточно данных для анализа"
+
+        start_rate = rates[0]
+        end_rate = rates[-1]
+        percentage_change = ((end_rate - start_rate) / start_rate) * 100
+        return f"{percentage_change:.2f}%"
+
+
+class CurrencyPlotter:
+    @staticmethod
+    def plot_currency_rates(dates, rates, base, symbol):
+        plt.style.use('dark_background')
+        plt.figure(figsize=(12, 6))
+        formatted_dates = [datetime.strptime(date, '%Y-%m-%d').strftime('%d.%m') for date in dates]
+
+        plt.plot(formatted_dates, rates, marker='o', linestyle='-', color='#1f77b4', label=f'{base} to {symbol}')
+        plt.title(f"Курс конвертации: {base} к {symbol}", fontsize=16, color='white')
+        plt.grid(visible=True, linestyle='--', alpha=0.5, color='gray')
+        plt.xticks(rotation=45, fontsize=10, color='white')
+        plt.yticks(fontsize=10, color='white')
+        plt.legend(fontsize=12, facecolor='black', edgecolor='white')
+
+        plot_path = "exchange_rate_plot.png"
+        plt.tight_layout()
+        plt.savefig(plot_path)
+        plt.close()
+        return plot_path
+
+
+class CurrencyBot:
+    def __init__(self, api_key, token):
+        self.currency_api = CurrencyAPI(api_key)
+        self.application = Application.builder().token(token).build()
+
+    async def start_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await update.message.reply_text(
+            "Здравствуйте, у нас вы можете узнать актуальные курсы валют.\n"
+            "Можете ознакомится с командами при помощи /help."
+        )
+
+    async def help_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        commands = (
+            "/start - Начните общение\n"
+            "/help - Список команд\n"
+            "/rate <FirstSymbol> <SecondSymbol> - Получить текущий курс\n"
+            "/history <FirstSymbol> <SecondSymbol> <Days> - Получить график за последние несколько дней\n"
+            "/currency - Получить список доступных валют"
+        )
+        await update.message.reply_text(commands)
+
+    async def rate_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        try:
+            base, symbol = context.args
+            rate = self.currency_api.get_currency_rate(base.upper(), symbol.upper())
+            if rate:
+                keyboard = [[InlineKeyboardButton("🔄Обновить курс", callback_data=f"update_{base}_{symbol}")]]
+                reply_markup = InlineKeyboardMarkup(keyboard)
+                await update.message.reply_text(
+                    text=f"Текущий курс конвертации {symbol.upper()} к {base.upper()}: {rate}",
+                    reply_markup=reply_markup
+                )
+            else:
+                await update.message.reply_text("Ошибка получения данных.")
+        except ValueError:
+            await update.message.reply_text("Пожалуйста, используйте заданный формат: /rate <FirstSymbol> <SecondSymbol>")
+        except Exception as e:
+            await update.message.reply_text(f"Error: {e}")
+
+    async def update_button(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        query = update.callback_query
+        await query.answer()
+
+        data = query.data.split("_")
+        if len(data) == 3 and data[0] == "update":
+            base, symbol = data[1], data[2]
+            rate = self.currency_api.get_currency_rate(base.upper(), symbol.upper())
+
+            if rate:
+                new_text = (
+                    f"Текущий курс конвертации {symbol.upper()} к {base.upper()}: {rate}\n"
+                    f"Последнее обновление: {datetime.now().strftime('%H:%M:%S')}"
+                )
+
+                keyboard = [[InlineKeyboardButton("🔄Обновить курс", callback_data=f"update_{base}_{symbol}")]]
+                reply_markup = InlineKeyboardMarkup(keyboard)
+
+                await query.edit_message_text(
+                    text=new_text,
+                    reply_markup=reply_markup
+                )
+            else:
+                await query.edit_message_text(
+                    text="Ошибка получения данных."
+                )
+
+    async def history_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        try:
+            base, symbol, days = context.args
+            days = int(days)
+            rates, dates = self.currency_api.get_historical_data(base.upper(), symbol.upper(), days)
+
+            if rates and dates:
+                plot_path = CurrencyPlotter.plot_currency_rates(dates, rates, base.upper(), symbol.upper())
+                trend = self.currency_api.calculate_trend(rates)
+                percentage_change = self.currency_api.calculate_percentage_change(rates)
+                caption = (f"График конвертации {base.upper()} в {symbol.upper()} "
+                           f"за последние {days} дней.\n"
+                           f"Тренд: {trend}.\n"
+                           f"Процентное изменение за {days} дней: {percentage_change}.")
+                await update.message.reply_photo(photo=open(plot_path, 'rb'), caption=caption)
+                os.remove(plot_path)
+            else:
+                await update.message.reply_text("Ошибка получения данных.")
+        except ValueError:
+            await update.message.reply_text("Пожалуйста, используйте заданный формат: /history <FirstSymbol> <SecondSymbol> <Days>")
+        except Exception as e:
+            await update.message.reply_text(f"Error: {e}")
+
+    async def currency_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        symbols = self.currency_api.get_available_currencies()
+        if symbols:
+            message = "Доступные валюты:\n" + "\n".join([f"{code}: {name}" for code, name in symbols.items()])
+            await update.message.reply_text(message)
         else:
             await update.message.reply_text("Ошибка получения данных.")
-    except ValueError:
-        await update.message.reply_text("Пожалуйста, используйте заданный формат: /rate <FirstSymbol> <SecondSymbol>")
-    except Exception as e:
-        await update.message.reply_text(f"Error: {e}")
 
-async def update_button(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    await query.answer()
+    def run(self):
+        self.application.add_handler(CommandHandler("start", self.start_command))
+        self.application.add_handler(CommandHandler("help", self.help_command))
+        self.application.add_handler(CommandHandler("rate", self.rate_command))
+        self.application.add_handler(CommandHandler("history", self.history_command))
+        self.application.add_handler(CommandHandler("currency", self.currency_command))
+        self.application.add_handler(CallbackQueryHandler(self.update_button))
+        self.application.run_polling()
 
-    data = query.data.split("_")
-    if len(data) == 3 and data[0] == "update":
-        base, symbol = data[1], data[2]
-        rate = get_currency_rate(base.upper(), symbol.upper())
-
-        if rate:
-            from datetime import datetime
-
-            new_text = (
-                f"Текущий курс конвертации {symbol.upper()} к {base.upper()}: {rate}\n"
-                f"Последнее обновление: {datetime.now().strftime('%H:%M:%S')}"
-            )
-
-            keyboard = [[InlineKeyboardButton("🔄Обновить курс", callback_data=f"update_{base}_{symbol}")]]
-            reply_markup = InlineKeyboardMarkup(keyboard)
-
-            await query.edit_message_text(
-                text=new_text,
-                reply_markup=reply_markup
-            )
-        else:
-            await query.edit_message_text(
-                text="Ошибка получения данных."
-            )
-
-
-async def history_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    try:
-        base, symbol, days = context.args
-        days = int(days)
-        rates, dates = get_historical_data(base.upper(), symbol.upper(), days)
-
-        if rates and dates:
-            plot_path = plot_currency_rates(dates, rates, base.upper(), symbol.upper())
-            caption = (f"График конвертации {base.upper()} в {symbol.upper()} "
-                       f"за последние {days} дней.")
-            await update.message.reply_photo(photo=open(plot_path, 'rb'), caption=caption)
-            os.remove(plot_path)
-        else:
-            await update.message.reply_text("Ошибка получения данных.")
-    except ValueError:
-        await update.message.reply_text("Пожалуйста, используйте заданный формат: /history <FirstSymbol> <SecondSymbol> <Days>")
-    except Exception as e:
-        await update.message.reply_text(f"Error: {e}")
-
-async def currency_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    symbols = get_available_currencies()
-    if symbols:
-        message = "Доступные валюты:\n" + "\n".join([f"{code}: {name}" for code, name in symbols.items()])
-        await update.message.reply_text(message)
-    else:
-        await update.message.reply_text("Ошибка получения данных.")
-
-async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "Здравствуйте, у нас вы можете узнать актуальные курсы валют.\n"
-        "Можете ознакомится с командами при помощи /help."
-    )
-
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    commands = (
-        "/start - Начните общение\n"
-        "/help - Список команд\n"
-        "/rate <FirstSymbol> <SecondSymbol> - Получить текущий курс\n"
-        "/history <FirstSymbol> <SecondSymbol> <Days> - Получить график за последние несколько дней\n"
-        "/currency - Получить список доступных валют"
-    )
-    await update.message.reply_text(commands)
-
-def main():
-    application = Application.builder().token(TOKEN).build()
-
-    application.add_handler(CommandHandler("start", start_command))
-    application.add_handler(CommandHandler("help", help_command))
-    application.add_handler(CommandHandler("rate", rate_command))
-    application.add_handler(CommandHandler("history", history_command))
-    application.add_handler(CommandHandler("currency", currency_command))
-    application.add_handler(CallbackQueryHandler(update_button))
-    application.run_polling()
 
 if __name__ == "__main__":
     print("Bot started...")
-    main()
+    bot = CurrencyBot(FIXER_API_KEY, TOKEN)
+    bot.run()
